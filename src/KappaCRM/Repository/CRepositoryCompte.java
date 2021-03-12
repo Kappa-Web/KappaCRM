286a029cd63d608a945d5d895d59312ca171e786
Reatir <bastien.fernandes.3g3@gmail.com>
2021-03-12 15:37:12 +0100

    Repository Compte

---
+++ src/KappaCRM/Repository/CRepositoryCompte.java
@@ -0,0 +1,15 @@
+package KappaCRM.Repository;
+
+import java.util.List;
+
+import KappaCRM.Model.CModelCompte;
+
+import java.sql.*;
+
+public class CRepositoryCompte {
+	
+	public List<CModelCompte> findAll() {
+		return null;
+		
+	}
+}
