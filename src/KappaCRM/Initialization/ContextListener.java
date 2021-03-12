8987b925bbc27f63f1cfe34dab6aa17ea0ab1c88
Reatir <bastien.fernandes.3g3@gmail.com>
2021-03-12 15:41:21 +0100

    Create ContextListener.java

---
+++ src/KappaCRM/Initialization/ContextListener.java
@@ -0,0 +1,43 @@
+package KappaCRM.Initialization;
+
+import java.sql.SQLException;
+
+import KappaCRM.Infrastructure.CInfraBasicConnectionPool;
+import KappaCRM.Utility.CUtilityStorage;
+import jakarta.servlet.ServletContextEvent;
+import jakarta.servlet.ServletContextListener;
+import jakarta.servlet.annotation.WebListener;
+
+@WebListener
+public class ContextListener implements ServletContextListener {
+
+
+	@Override
+	public void contextDestroyed(ServletContextEvent arg0) {
+		// TODO Auto-generated method stub
+		System.out.println("Shutting down!");
+		try {
+			CUtilityStorage.getBasicConnectionPool().shutdown();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public void contextInitialized(ServletContextEvent arg0) {
+		// TODO Auto-generated method stub
+		  System.out.println("Starting up!");
+		  try {
+			Class.forName("com.mysql.cj.jdbc.Driver");  
+			CUtilityStorage.setBasicConnectionPool(CInfraBasicConnectionPool.create("jdbc:mysql://localhost:3306/projet_kappa", "root", ""));
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+}
