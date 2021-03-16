74834b4ab4cdd333193630a9ca0c952479328e96
Reatir <bastien.fernandes.3g3@gmail.com>
2021-03-16 17:06:18 +0100

    Update CRepositoryCompte.java

--- src/KappaCRM/Repository/CRepositoryCompte.java
+++ src/KappaCRM/Repository/CRepositoryCompte.java
@@ -3,6 +3,7 @@ package KappaCRM.Repository;
 import java.util.List;
 
 import KappaCRM.Model.CModelCompte;
+import KappaCRM.Utility.CUtilityStorage;
 
 import java.sql.*;
 
@@ -11,5 +12,30 @@ public class CRepositoryCompte {
 	public List<CModelCompte> findAll() {
 		return null;
 		
+	}
+	
+	public boolean existValidByIdAndPassword(CModelCompte compte) throws SQLException
+	{
+		
+		Connection conn;
+		try {
+			conn = CUtilityStorage.getBasicConnectionPool().getConnection();
+			PreparedStatement state = conn.prepareStatement("SELECT EXISTS(SELECT * FROM "
+					+ "comptes WHERE `id` =  ? AND mot_de_passe = ?)");
+			
+			state.setLong(1, compte.getId());
+			state.setString(2, compte.getMotDePasse());
+			
+			boolean result = state.execute();
+			CUtilityStorage.getBasicConnectionPool().releaseConnection(conn);
+			
+			return result;
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			CUtilityStorage.getBasicConnectionPool().releaseConnection(conn);
+			return false;
+		}
 	}
 }
