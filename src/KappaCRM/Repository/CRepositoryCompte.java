8218dc9ee550e860ab42a8f2abecf95ac8398152
Reatir <bastien.fernandes.3g3@gmail.com>
2021-03-22 19:05:15 +0100

    Update CRepositoryCompte.java

--- src/KappaCRM/Repository/CRepositoryCompte.java
+++ src/KappaCRM/Repository/CRepositoryCompte.java
@@ -10,9 +10,54 @@ import java.sql.*;
 public class CRepositoryCompte {
 	
 	public List<CModelCompte> findAll() {
+		
+		
+		Connection conn;
+		
+		/*	conn = CUtilityStorage.getBasicConnectionPool().getConnection();
+			PreparedStatement state = conn.prepareStatement("SELECT EXISTS(SELECT * FROM "
+					+ "comptes WHERE `username` =  ? AND mot_de_passe = ?) as result");
+			
+			
+			
+			ResultSet result = state.executeQuery();	
+			
+			CUtilityStorage.getBasicConnectionPool().releaseConnection(conn);
+			result.next();*/
+			return null;			
+		
+	}
+	
+	public CModelCompte findById(CModelCompte compte) {
+		
 		return null;
 		
 	}
+	
+	public CModelCompte findByUsername(CModelCompte compte) throws SQLException {
+		
+		Connection conn;
+		
+		conn = CUtilityStorage.getBasicConnectionPool().getConnection();
+		PreparedStatement state = conn.prepareStatement("SELECT * FROM comptes WHERE `username` =  ? AND mot_de_passe = ?");
+
+		state.setString(1, compte.getIdentifiant());
+		state.setString(2, compte.getMotDePasse());
+		
+		ResultSet result = state.executeQuery();
+		
+		CUtilityStorage.getBasicConnectionPool().releaseConnection(conn);
+		
+		result.next();
+		
+		compte.setId(result.getInt(1));
+		compte.setTypeCompte(result.getString(4));
+		
+		return compte;
+		
+	}
+	
+	
 	
 	public boolean existValidByUsernameAndPassword(CModelCompte compte) throws SQLException
 	{
