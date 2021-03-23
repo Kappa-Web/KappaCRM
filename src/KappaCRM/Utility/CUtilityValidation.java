f648d0e021312e5278d72ecd0449ef2cf5e1689e
Reatir <bastien.fernandes.3g3@gmail.com>
2021-03-23 19:01:09 +0100

    Update CUtilityValidation.java

--- src/KappaCRM/Utility/CUtilityValidation.java
+++ src/KappaCRM/Utility/CUtilityValidation.java
@@ -1,5 +1,11 @@
 package KappaCRM.Utility;
 
+import KappaCRM.Model.CModelCompte;
+import jakarta.servlet.http.HttpSession;
+
 public class CUtilityValidation {
 
+	public static boolean CompteIsValid(HttpSession session) {
+		return session.getAttribute("compte") != null || ((CModelCompte)session.getAttribute("compte")).getId() != 0;
+	}
 }
