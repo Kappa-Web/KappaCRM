0262e87d45c508fb60873edb0126f4c1305614b9
Reatir <bastien.fernandes.3g3@gmail.com>
2021-03-23 21:06:55 +0100

    Update CRepositoryEntity.java

--- src/KappaCRM/Repository/CRepositoryEntity.java
+++ src/KappaCRM/Repository/CRepositoryEntity.java
@@ -1,6 +1,42 @@
 package KappaCRM.Repository;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import KappaCRM.Model.CModelEntity;
+import KappaCRM.Utility.CUtilityStorage;
+
 public class CRepositoryEntity {
 
-	public 
+	public CModelEntity findById(long id) throws SQLException {
+		
+		Connection conn;
+		
+		conn = CUtilityStorage.getBasicConnectionPool().getConnection();
+		PreparedStatement state = conn.prepareStatement("SELECT * FROM entite WHERE `id` =  ? ");
+
+		state.setLong(1,id);
+		
+		
+		ResultSet result = state.executeQuery();
+		
+		CUtilityStorage.getBasicConnectionPool().releaseConnection(conn);
+		
+		CModelEntity entity = new CModelEntity();
+		
+		if(result.next()) {
+		
+			entity.setId(result.getLong(1));
+			entity.setNom(result.getString(2));
+			entity.setPremon(result.getString(3));
+			entity.setCivilite(result.getString(4));
+			
+			
+		}
+		
+		
+		return entity;
+	}
 }
