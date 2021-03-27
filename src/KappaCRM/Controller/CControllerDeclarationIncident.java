f32e7e7b033d6b1ea59236befa70e31a6f86a4bb
Reatir <bastien.fernandes.3g3@gmail.com>
2021-03-27 14:17:58 +0100

    Create CControllerDeclarationIncident.java

---
+++ src/KappaCRM/Controller/CControllerDeclarationIncident.java
@@ -0,0 +1,28 @@
+package KappaCRM.Controller;
+
+import java.io.IOException;
+
+import KappaCRM.Utility.CUtilityValidation;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+@SuppressWarnings("serial")
+public class CControllerDeclarationIncident extends HttpServlet {
+
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		HttpSession session = request.getSession();
+
+		if (CUtilityValidation.CompteIsValid(session)) {
+			this.getServletContext().getRequestDispatcher("/WEB-INF/DeclarationIncident.jsp").forward(request,
+					response);
+		} else {
+			response.sendRedirect("/KappaCRM");
+		}
+
+	}
+
+}
