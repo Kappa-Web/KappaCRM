b9c7b344973c6445d0763a331943e842237a2bf3
Reatir <bastien.fernandes.3g3@gmail.com>
2021-03-27 21:06:55 +0100

    Update CRepositoryIncident.java

--- src/KappaCRM/Repository/CRepositoryIncident.java
+++ src/KappaCRM/Repository/CRepositoryIncident.java
@@ -1,8 +1,10 @@
 package KappaCRM.Repository;
 
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 
 import KappaCRM.Model.CModelEntity;
 import KappaCRM.Model.CModelIncident;
@@ -10,7 +12,7 @@ import KappaCRM.Utility.CUtilityStorage;
 
 public class CRepositoryIncident {
 	
-	public void insert(CModelIncident incident) {
+	public void insert(CModelIncident incident) throws SQLException {
 		
 		
 		Connection conn;
@@ -19,13 +21,19 @@ public class CRepositoryIncident {
 		PreparedStatement state = conn.prepareStatement("INSERT INTO `incident`(`libelle`, `description`,"
 				+ " `date_ajout`, `date_modif`, `date_fin`, `lieu`, `statut`, `fk_id_entite_superviseur`,"
 				+ " `fk_id_mission`, `fk_id_entite_declarant`) VALUES"
-				+ "(?,?,?,?,?,?,?,?,?,?)");
-
-		state.setLong(1,id);
+				+ "(?,?,CURRENT_TIMESTAMP,CURRENT_TIMESTAMP,null,?,?,null,null,null)");
+		System.out.print("pute");
+		
+		state.setString(1,incident.getLibelle());
+		state.setString(2,incident.getDescription());		
+		state.setString(3, incident.getLieu());
+		state.setString(4,incident.getStatut());
+		//state.setLong(5, incident.getFkIdEntiteSuperviseur());
+		//state.setLong(6, incident.getFkIdEntiteDeclarant());
 		
 		
-		ResultSet result = state.executeQuery();
-		
+		state.execute();
+		System.out.print("putain");
 		CUtilityStorage.getBasicConnectionPool().releaseConnection(conn);
 	
 	}
