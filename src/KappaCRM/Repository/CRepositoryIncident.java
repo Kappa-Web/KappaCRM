7753dfd4dc452cb9389f542553f95704b93c5179
Reatir <bastien.fernandes.3g3@gmail.com>
2021-03-28 18:42:14 +0200

    Update CRepositoryIncident.java

--- src/KappaCRM/Repository/CRepositoryIncident.java
+++ src/KappaCRM/Repository/CRepositoryIncident.java
@@ -21,19 +21,18 @@ public class CRepositoryIncident {
 		PreparedStatement state = conn.prepareStatement("INSERT INTO `incident`(`libelle`, `description`,"
 				+ " `date_ajout`, `date_modif`, `date_fin`, `lieu`, `statut`, `fk_id_entite_superviseur`,"
 				+ " `fk_id_mission`, `fk_id_entite_declarant`) VALUES"
-				+ "(?,?,CURRENT_TIMESTAMP,CURRENT_TIMESTAMP,null,?,?,null,null,null)");
-		System.out.print("pute");
+				+ "(?,?,CURRENT_TIMESTAMP,CURRENT_TIMESTAMP,null,?,?,null,null,?)");
+		
 		
 		state.setString(1,incident.getLibelle());
 		state.setString(2,incident.getDescription());		
 		state.setString(3, incident.getLieu());
-		state.setString(4,incident.getStatut());
-		//state.setLong(5, incident.getFkIdEntiteSuperviseur());
-		//state.setLong(6, incident.getFkIdEntiteDeclarant());
+		state.setString(4,incident.getStatut());	
+		state.setLong(5, incident.getFkIdEntiteDeclarant());
 		
 		
 		state.execute();
-		System.out.print("putain");
+	
 		CUtilityStorage.getBasicConnectionPool().releaseConnection(conn);
 	
 	}
