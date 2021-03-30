b14ba0e64d7a19d9e43047d62b5e24bf906da0f7
Reatir <bastien.fernandes.3g3@gmail.com>
2021-03-30 22:03:02 +0200

    Update CControllerLitige.java

--- src/KappaCRM/Controller/CControllerLitige.java
+++ src/KappaCRM/Controller/CControllerLitige.java
@@ -4,11 +4,12 @@ import java.io.IOException;
 
 import KappaCRM.Utility.CUtilityValidation;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 
-public class CControllerLitige {
+public class CControllerLitige extends HttpServlet{
 	
 	
 	public void doGet( HttpServletRequest request, HttpServletResponse response ) throws ServletException, IOException{
