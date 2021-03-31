4845e01838c0b59ae061d6937a51fc7be5421c22
Reatir <bastien.fernandes.3g3@gmail.com>
2021-03-31 19:44:02 +0200

    Create CModelLitige.java

---
+++ src/KappaCRM/Model/CModelLitige.java
@@ -0,0 +1,10 @@
+package KappaCRM.Model;
+
+public class CModelLitige {
+
+	private long id;
+	private String libelle;
+	private byte[] photo;
+	private long fkIdCrise;
+	private long fkIdEntite;
+}
