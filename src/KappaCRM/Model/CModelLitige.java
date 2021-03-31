243d918e0d3ac58f40aa1d39fcb41382515f261e
Reatir <bastien.fernandes.3g3@gmail.com>
2021-03-31 20:02:38 +0200

    Update CModelLitige.java

--- src/KappaCRM/Model/CModelLitige.java
+++ src/KappaCRM/Model/CModelLitige.java
@@ -7,4 +7,39 @@ public class CModelLitige {
 	private byte[] photo;
 	private long fkIdCrise;
 	private long fkIdEntite;
+	
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getLibelle() {
+		return libelle;
+	}
+	public void setLibelle(String libelle) {
+		this.libelle = libelle;
+	}
+	public byte[] getPhoto() {
+		return photo;
+	}
+	public void setPhoto(byte[] photo) {
+		this.photo = photo;
+	}
+	public long getFkIdCrise() {
+		return fkIdCrise;
+	}
+	public void setFkIdCrise(long fkIdCrise) {
+		this.fkIdCrise = fkIdCrise;
+	}
+	public long getFkIdEntite() {
+		return fkIdEntite;
+	}
+	public void setFkIdEntite(long fkIdEntite) {
+		this.fkIdEntite = fkIdEntite;
+	}
+	
+	
+	
 }
