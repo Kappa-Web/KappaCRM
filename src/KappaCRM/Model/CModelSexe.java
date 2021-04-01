485a3d13ae9f2cfa63e5c4b2b40499bd38c20b62
Reatir <bastien.fernandes.3g3@gmail.com>
2021-04-01 18:48:31 +0200

    Create CModelSexe.java

---
+++ src/KappaCRM/Model/CModelSexe.java
@@ -0,0 +1,21 @@
+package KappaCRM.Model;
+
+public class CModelSexe {
+
+	private long id;
+	private String libelle;
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getLibelle() {
+		return libelle;
+	}
+	public void setLibelle(String libelle) {
+		this.libelle = libelle;
+	}
+
+}
