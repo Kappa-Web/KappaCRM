aa8dcd66b47a71532beb250f82f1401437d2173d
Reatir <bastien.fernandes.3g3@gmail.com>
2021-04-05 14:09:21 +0200

    Update CServiceEntite.java

--- src/KappaCRM/Service/CServiceEntite.java
+++ src/KappaCRM/Service/CServiceEntite.java
@@ -3,7 +3,9 @@ package KappaCRM.Service;
 import java.sql.SQLException;
 
 import KappaCRM.Model.CModelEntity;
+import KappaCRM.Repository.CRepositoryCivilite;
 import KappaCRM.Repository.CRepositoryEntity;
+import KappaCRM.Repository.CRepositorySexe;
 
 public class CServiceEntite {
 
@@ -11,10 +13,26 @@ public class CServiceEntite {
 		CRepositoryEntity repositoryEntity = new CRepositoryEntity();
 		try {
 			repositoryEntity.insert(entite);
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
+		} catch (SQLException e) {			
 			e.printStackTrace();
 		}
 		
+	}
+	
+	public CModelEntity findById(long id)
+	{
+		CRepositoryEntity repositoryEntity  = new CRepositoryEntity();
+		CRepositoryCivilite repositoryCivilite = new CRepositoryCivilite();
+		CRepositorySexe repositorySexe = new CRepositorySexe();
+		 try {
+			CModelEntity entite = repositoryEntity.findById(id);			
+			entite.setCivilite(repositoryCivilite.findById(entite.getFk_civilite()).getLibelle());
+			entite.setSexe(repositorySexe.findById(entite.getFk_sexe()).getLibelle());
+			return entite;
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return null;
+		}
 	}
 }
